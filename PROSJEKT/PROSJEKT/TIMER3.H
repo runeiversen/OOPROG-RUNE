0ed218c7213c8d101ff2faf7a6cd45077a703278
runeiversen <runeiversen@hotmail.com>
2014-03-24 13:34:49 +0100

    Prosjekt
    Hey

---
+++ PROSJEKT/PROSJEKT/TIMER3.H
@@ -0,0 +1,22 @@
+﻿#if !defined(__TIMER3_H) //  Sikrer at filen kun inkluderes en gang:
+#define __TIMER3_H
+
+#include <ctime>        //  Struct'en "tm".
+
+
+class Timer  {
+  private:
+    int    dagerPrMaaned[12];  //  Antall dager i hver av �rets 12 m�neder.
+    tm*    tidspunkt;          //  Peker til 'tm'-struct.
+    time_t tid;                //  Avlest tid lagres i denne. (tidligere: 'long')
+    bool   skuddaar(int aa);   //  3x private/skjulte hjelpefunksjoner:
+    int    dagnummer(int da, int ma, int aa);
+
+  public:
+    Timer();
+    int  hentDato();
+    int  forskjellDatoer(int t1, int t2);
+    void konverter(int t, int& da, int& ma, int& aa);
+};
+
+#endif
